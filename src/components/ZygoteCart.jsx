f866022fd0e34b348fe5a606e0f8c03e9a548b8d
Jacob <jgsimmerman@gmail.com>
2020-04-23 00:56:33 -0400

    cloudflare workers?

--- src/components/ZygoteCart.jsx
+++ src/components/ZygoteCart.jsx
@@ -31,19 +31,21 @@ const ZygoteCart = props => {
   return(
     
     <Cart
-     stripeApiKey="pk_live_nMctV4G1movcajzQsjXet2Zs000zybvAUV"
-     //stripeApiKey="pk_test_Vc8z3p2pdxHFQgxhbbhIXtyv00GnPddsjV"
+     //stripeApiKey="pk_live_nMctV4G1movcajzQsjXet2Zs000zybvAUV"
+     stripeApiKey="pk_test_Vc8z3p2pdxHFQgxhbbhIXtyv00GnPddsjV"
 
      // stripeApiKey={process.env.STRIPE_API_PUBLIC}
 
-      infoWebhook='/.netlify/functions/info-stripe'
-      orderWebhook='/.netlify/functions/order-stripe'
-      shippingWebhook='/.netlify/functions/shipping-stripe'
+     // infoWebhook='/.netlify/functions/info-stripe'
+     // orderWebhook='/.netlify/functions/order-stripe'
+      //shippingWebhook='/.netlify/functions/shipping-stripe'
 
       // infoWebhook='https://97bc9b72.ngrok.io/.netlify/functions/info-stripe'
       // orderWebhook='https://97bc9b72.ngrok.io/.netlify/functions/order-stripe'
       // shippingWebhook='https://97bc9b72.ngrok.io/.netlify/functions/shipping-stripe'
-
+      infoWebhook='https://my-router-app.jgsimmerman.workers.dev/info-stripe'
+      orderWebhook='https://my-router-app.jgsimmerman.workers.dev/order-stripe'
+      shippingWebhook='https://my-router-app.jgsimmerman.workers.dev/shipping-stripe'
       // infoWebhook={process.env.INFO_WEBHOOK}
       // orderWebhook={process.env.ORDER_WEBHOOK}
       // shippingWebhook={process.env.SHIPPING_WEBHOOK}
